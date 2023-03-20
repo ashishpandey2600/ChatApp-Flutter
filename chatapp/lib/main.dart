f79bda835da79fa278b5f6c8558d2d60a8d2698c
ashishpandey2600 <ashishpandey@Ashishs-MacBook-Pro.local>
2023-03-20 20:38:54 +0530

    added Firebase requirements for AndroidIosWeb

---
+++ chatapp/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:chatapp/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(apiKey: "AIzaSyDny6AIjtCZAfdLWMkZSgApkFe0jGOffTQ",
+  authDomain: Constants.authDomain,
+  projectId: Constants.projectId,
+  storageBucket: Constants.storageBucket,
+  messagingSenderId: Constants.messagingSenderId,
+  appId: Constants.appId,
+  measurementId: Constants.measurementId)
+  
+  );
+  } else {
+    
+  await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ chatapp/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static String authDomain = "chatapp-5c35a.firebaseapp.com";
+  static String storageBucket = "chatapp-5c35a.appspot.com";
+  static String appId = "1:553209416106:web:27d8df7513dcc03ac183d3";
+  static String apiKey = "AIzaSyDny6AIjtCZAfdLWMkZSgApkFe0jGOffTQ";
+  static String messagingSenderId = "553209416106";
+  static String projectId = "chatapp-5c35a";
+  static String measurementId = "G-Y5EC75L61V";
+}
